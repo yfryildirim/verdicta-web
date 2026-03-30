e4b8fcc549cc0ab0308aeb2c51b69d3dca966f6c
yfryildirim <yfryildirim@gmail.com>
2026-03-30 14:44:37 +0300

    feat: add App Links (Android) and Universal Links (iOS) well-known files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
